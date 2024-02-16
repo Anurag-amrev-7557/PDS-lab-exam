e16790ad65f3ff4d66c36f9b577a2a042895329a
Anurag Verma <mindreaders7557@gmail.com>
2024-02-16 16:03:47 +0530

    ⠀

--- 23CE01006_setB_qsn2.c
+++ 23CE01006_setB_qsn2.c
@@ -1,16 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-    int lowerLimit, upperLimit;
-    printf("Enter the lower limit: ");
-    scanf("%d", &lowerLimit);
-
-    printf("Enter the upper limit: ");
-    scanf("%d", &upperLimit);
-
-    for(int i=lowerLimit; i<=upperLimit; i++) {
+int main(){
+    int a,i=1,j=1,sum=0;
+    printf("Enter the number :");
+    scanf("%d",&a);
+    int arr[15]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+   
+   while (a%i==0 && i<a)
+    {
+        arr[j-1]=i;
+        sum=sum+arr[j-1];
+        i=i+1;
+        j=j+1;
         
-    }    
 
-    return 0;
+    }
+   if (sum==a)
+   {
+    printf("%d is a perfect number",a);
+   }
+   else {
+    printf("%d is not a perfect number",a);
+   }
+   
+    
+    
+   return 0;
 }
